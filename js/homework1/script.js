3b1cb4ae88f7fd7163c37264d236f763cdd6b906
User <savchak181998@gmail.com>
2022-11-01 17:20:25 +0200

    aaa

---
+++ js/homework1/script.js
@@ -0,0 +1,33 @@
+// let q = "hello";
+// let w = "owu";
+// let e = "com";
+// let r = "ua";
+// let t = 1;
+// let y = 10;
+// let u = -999;
+// let i = 123;
+// let o = 3.14;
+// let p = 2.7;
+// let a = 16;
+// let s = true;
+// let d = false;
+// console.log(q,w,e,r,t,y,u,i,o,p,a,s,d);
+
+// let firstName = "Савчак ";
+// let middleName = "Роман ";
+// let lastName = "Михайлович";
+// let person = firstName + middleName + lastName;
+// console.log(person);
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// let firstName = prompt("firstName");
+// let middleName = prompt("middleName");
+// let lastName = prompt("lastName");
+// let person = firstName + " " + middleName + " " + lastName;
+// console.log(person);
