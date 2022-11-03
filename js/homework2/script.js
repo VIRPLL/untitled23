7cac9794f7562b8e01da7e7f182a2d51d0035ab2
User <savchak181998@gmail.com>
2022-11-03 19:01:30 +0200

    aaa

---
+++ js/homework2/script.js
@@ -0,0 +1,157 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arr = [1, 2, 3, 4, 5, "a", "w", "e", "r", "t"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+// console.log(arr[9]);
+//
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let b1 = {
+//     title : "Містер мерседес",
+//     pageCount : 350,
+//     genre : "Детектив"
+// };
+//
+// let b2 = {
+//     title : "Воно",
+//     pageCount : 950,
+//     genre : "Хорор"
+// };
+//
+// let b3 = {
+//     title : "Звязок",
+//     pageCount : 350,
+//     genre : "Хорор"
+// };
+
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори" - масив. Кожен автор має поля name,age
+//
+// let b1 = {
+//     title : "Містер мерседес",
+//     pageCount : 350,
+//     genre : "Детектив",
+//     author : [
+//         {
+//             name : "stiven",
+//             age : 45
+//         },
+//         {
+//             name : "adam",
+//             age : 44
+//         }
+//     ]
+// };
+//
+// let b2 = {
+//     title : "Воно",
+//     pageCount : 950,
+//     genre : "Хорор",
+//     author : [
+//         {
+//             name : "stiven",
+//             age : 45
+//         },
+//         {
+//             name : "adam",
+//             age : 44
+//         }
+//     ]
+// };
+//
+// let b3 = {
+//     title : "Звязок",
+//     pageCount : 350,
+//     genre : "Хорор",
+//     author : [
+//         {
+//             name : "stiven",
+//             age : 45
+//         },
+//         {
+//             name : "adam",
+//             age : 44
+//         }
+//     ]
+// };
+
+
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.
+// Вивести в консоль пароль кожного користувача
+
+// let users = [
+//     {
+//         name : "Roman",
+//         username : "awwd",
+//         password : "iamdqp"
+//     }  ,
+//     {
+//         name : "Adam",
+//         username : "tdhrh",
+//         password : "mdgfx"
+//     }  ,
+//     {
+//         name : "Sergry",
+//         username : "ketsrt",
+//         password : "wsjtrfzhd"
+//     }  ,
+//     {
+//         name : "Petro",
+//         username : "jftsh",
+//         password : "ltyrkud"
+//     }  ,
+//     {
+//         name : "Nastya",
+//         username : "rlkdt",
+//         password : "jdrt"
+//     }  ,
+//     {
+//         name : "Olya",
+//         username : "jytfg",
+//         password : "jrtfd"
+//     }  ,
+//     {
+//         name : "Greg",
+//         username : "JRST",
+//         password : "35yehts"
+//     }  ,
+//     {
+//         name : "Roman",
+//         username : "wTES",
+//         password : ";uifyldkts"
+//     }  ,
+//     {
+//         name : "Oleg",
+//         username : "aehh",
+//         password : "usrtf"
+//     }  ,
+//     {
+//         name : "Petro",
+//         username : "kdtysj",
+//         password : "gwsdf"
+//     }
+// ];
+//
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
